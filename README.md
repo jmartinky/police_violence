df7b83ce9b213fdf028896eb5802b9d78391870d
Justin A. Martin <160555180+jmartinky@users.noreply.github.com>
2024-03-29 17:20:32 -0500

    various updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 2024-03-28 copied Mapping Police Violence from mappingpoliceviolence.org
 2024-03-28 downloaded Table_10 offenses known to LE by state by county
+2024-03-19 downloaded https://www2.census.gov/programs-surveys/popest/datasets/2020-2023/counties/totals/co-est2023-alldata.csv
+
--- violence_EDA.py
+++ violence_EDA.py
@@ -2,8 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
-#df = pd.read_excel('data.xlsx')
-#print(df.info())
+violence = pd.read_excel('data.xlsx')
+#violence['StateCounty'] = violence[['state', 'county']].agg(','.join, axis=1 )
+#print(violence.info())
 
-df_2 = pd.read_excel('Table_10_Offenses_Known_to_Law_Enforcement_by_State_by_Metropolitan_and_Nonmetropolitan_Counties_2022.xlsx')
-print(df_2.info())
+states = pd.read_csv('states.csv')
+#print(states)
+
+violence_states = pd.merge(violence, states, on='state', how='left')
+print(violence_states.head())
+
+#offenses = pd.read_excel('Table_10_Offenses_Known_to_Law_Enforcement_by_State_by_Metropolitan_and_Nonmetropolitan_Counties_2022.xlsx')
+#offenses['StateCounty'] = offenses[['State', 'County']].agg(','.join, axis=1)
+#print(offenses.head())
+
