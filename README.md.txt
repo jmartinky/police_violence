3189c9ba22efaad2096037f0c5425415ab411c8a
Justin A. Martin <160555180+jmartinky@users.noreply.github.com>
2024-03-29 12:36:33 -0500

    Initial commit

---
+++ README.md.txt
@@ -0,0 +1,2 @@
+2024-03-28 copied Mapping Police Violence from mappingpoliceviolence.org
+2024-03-28 downloaded Table_10 offenses known to LE by state by county
---
+++ violence_EDA.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+#df = pd.read_excel('data.xlsx')
+#print(df.info())
+
+df_2 = pd.read_excel('Table_10_Offenses_Known_to_Law_Enforcement_by_State_by_Metropolitan_and_Nonmetropolitan_Counties_2022.xlsx')
+print(df_2.info())
